349e7c51e58ea482c4436ca64e112e7bb2d81277
WenerRodrigo <wener19.wr@gmail.com>
2024-06-26 22:31:58 -0300

    feat: add restaurant list component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -34,8 +35,8 @@ const Home = async () => {
       </div>
 
       <div className="px-5 pt-6">
-        <PromoBanner 
-          src="/promo-banner.png" 
+        <PromoBanner
+          src="/promo-banner.png"
           alt="Até 30% de desconto em pizzas"
         />
       </div>
@@ -51,14 +52,23 @@ const Home = async () => {
             <ChevronRightIcon size={16} />
           </Button>
         </div>
-        <ProductList products={products}/>
-
-        <div className="px-5 pt-6">
-        <PromoBanner 
-          src="/banner02.png" 
-          alt="A partir de R$17,90 em lanches"
-        />
+        <ProductList products={products} />
       </div>
+      <div className="px-5 pt-6">
+        <PromoBanner src="/banner02.png" alt="A partir de R$17,90 em lanches" />
+      </div>
+      <div className="space-y-4 pt-6 py-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </>
   );
